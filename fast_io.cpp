b94980c9eba5f9ad9d0de728a06e345ed56780e6
wawaroutas <alexvava@csd.auth.gr>
2021-11-15 00:41:16 +0200

    added microsoft read and write version in case of msvc compilation

--- fast_io.cpp
+++ fast_io.cpp
@@ -2,12 +2,16 @@
 #include <unistd.h> // read(), write()
 #define READ(buff, len) read(0, buff, len)
 #define WRITE(buff, len) write(1, buff, len)
+#elif defined(_MSC_VER) && !defined(__INTEL_COMPILER)
+#include <io.h>     // _read(), _write()
+#define READ(buff, len) _read(0, buff, len)
+#define WRITE(buff, len) _write(1, buff, len)
 #else
 #include <stdio.h>  // fread(), fwrite(), stdin, stdout
 #if defined(_BSD_SOURCE) || defined(_SVID_SOURCE)
 #define READ(buff, len) fread_unlocked(buff, sizeof(char), len, stdin)
 #define WRITE(buff, len) fwrite_unlocked(buff, sizeof(char), len, stdout)
-#elif defined(_CRT_DISABLE_PERFCRIT_LOCKS) 
+#elif defined(_CRT_DISABLE_PERFCRIT_LOCKS)
 #define READ(buff, len) _fread_nolock(buff, sizeof(char), len, stdin)
 #define WRITE(buff, len) _fwrite_nolock(buff, sizeof(char), len, stdout)
 #elif
@@ -29,12 +33,12 @@ class IO {
   IO(IO&&) = delete;
   IO& operator=(IO&&) = delete;
 
-  template<typename T, bool> T read();
-  template<typename T> void read(T&);
-  template<typename T, typename... Args> void read(T&, Args&...);
+  // template<typename T, bool> T read();
+  // template<typename T> void read(T&);
+  // template<typename T, typename... Args> void read(T&, Args&...);
 
-  template<typename T, bool> void write(T);
-  template<typename T, typename... Args> void write(T, Args...);
+  // template<typename T, bool> void write(T);
+  // template<typename T, typename... Args> void write(T, Args...);
  private:
   char buffer_[buffer_size] {};
   char* pos_ { buffer_ };
